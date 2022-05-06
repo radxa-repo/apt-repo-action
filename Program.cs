5477a1bd5be9eb9567bf261ea197b0b29c65de69
Yuntian Zhang <yt@radxa.com>
2022-05-06 16:45:22 +0800

    Allow selectively excluding some artifacts in GitHub release

--- Program.cs
+++ Program.cs
@@ -2,37 +2,48 @@
 using System.Diagnostics;
 using Octokit;
 using static System.Console;
+using System.Text.RegularExpressions;
 
 class JsonHelper
 {
+    public static async Task<T?> DeserializeJson<T>(Stream? s)
+    {
+        return s is null || s.Length == 0 ? default(T) : await JsonSerializer.DeserializeAsync<T>(s);
+    }
+
     public static async Task<T?> DeserializeJson<T>(string FilePath)
     {
         using var f = File.Open(FilePath, System.IO.FileMode.OpenOrCreate);
-        return f.Length == 0 ? default(T) : await JsonSerializer.DeserializeAsync<T>(f);
+        return await DeserializeJson<T>(f);
     }
 
-    public static async Task SerializeJson<T>(string FilePath, T Object)
+    public static async Task SerializeJson<T>(Stream s, T obj)
+    {
+        await JsonSerializer.SerializeAsync(s, obj, typeof(T), new JsonSerializerOptions { WriteIndented = true });
+    }
+
+    public static async Task SerializeJson<T>(string FilePath, T obj)
     {
         using var f = File.Open(FilePath, System.IO.FileMode.OpenOrCreate);
-        await JsonSerializer.SerializeAsync(f, Object, typeof(T), new JsonSerializerOptions { WriteIndented = true });
+        await SerializeJson(f, obj);
     }
 }
 
-class Config
+class AptConfig
 {
     const string LOCAL_CONFIG = ".apt-repo.json";
     
-    public Config()
+    public AptConfig()
     {
         Releases = new List<string>();
     }
 
-    public static async Task<Config> GetConfig()
+    public static async Task<AptConfig> GetConfig()
     {
-        var conf = await JsonHelper.DeserializeJson<Config>(LOCAL_CONFIG);
+        var conf = await JsonHelper.DeserializeJson<AptConfig>(LOCAL_CONFIG);
         if (conf is null)
         {
-            conf = new Config();
+            conf = new AptConfig();
             conf.Releases.Add("stable");
             await JsonHelper.SerializeJson(LOCAL_CONFIG, conf);
             WriteLine($"Example config file has been generated. Please adapt it to your environment and try again.");
@@ -45,27 +56,65 @@ class Config
     public List<string> Releases { get; set; }
 }
 
+class PkgConfig
+{
+    public PkgConfig()
+    {
+        Exclude = new List<string>();
+    }
+
+    public static async Task<PkgConfig> GetConfig(ReleaseAsset a)
+    {
+        using var s = await Program.DownloadFile(a.BrowserDownloadUrl);
+
+        var conf = await JsonHelper.DeserializeJson<PkgConfig>(s);
+        if (conf is null)
+        {
+            conf = new PkgConfig();
+        }
+        
+        return conf;
+    }
+
+    public List<string> Exclude { get; set; }
+
+    public bool IsExcluded(ReleaseAsset asset)
+    {
+        return Exclude.AsParallel().Any(i => Regex.Match(asset.Name, i).Success);
+    }
+}
+
 class Program
 {
     const string LOCAL_PKG_LIST = "pkgs.json";
 
     static HttpClient _http = new HttpClient();
-    static Config _conf = new Config();
+    static AptConfig _aptconf = new AptConfig();
     static Dictionary<string, string> _pkgs = new Dictionary<string, string>();
 
-    static async Task<bool> DownloadAsset(ReleaseAsset asset)
+    public static async Task<Stream> DownloadFile(string url)
     {
-        WriteLine($"Downloading {asset.Name}");
-        var path = Path.Combine(Path.GetTempPath(), "apt-repo", asset.Name);
-        using (var ds = await _http.GetStreamAsync(asset.BrowserDownloadUrl))
+        return await _http.GetStreamAsync(url);
+    }
+
+    public static async Task DownloadFile(string url, string path)
+    {
+        using (var ds = await DownloadFile(url))
         using (var df = File.OpenWrite(path))
         {
             await ds.CopyToAsync(df);
             await df.FlushAsync();
         }
+    }
+
+    static async Task<bool> DownloadAsset(ReleaseAsset asset)
+    {
+        WriteLine($"Downloading {asset.Name}");
+        var path = Path.Combine(Path.GetTempPath(), "apt-repo", asset.Name);
+        await DownloadFile(asset.BrowserDownloadUrl, path);
 
         var flag = true;
-        await Parallel.ForEachAsync(_conf.Releases, async (release, token) => {
+        await Parallel.ForEachAsync(_aptconf.Releases, async (release, token) => {
             using var p = Process.Start(new ProcessStartInfo() {
                 FileName = "/bin/bash",
                 ArgumentList = {"-c", $"freight add -e {path} apt/{release}"},
@@ -118,7 +167,7 @@ class Program
             return -2;
         }
 
-        _conf = await Config.GetConfig();
+        _aptconf = await AptConfig.GetConfig();
 
         _pkgs = await JsonHelper.DeserializeJson<Dictionary<string, string>>(LOCAL_PKG_LIST) ?? new Dictionary<string, string>();
 
@@ -129,13 +178,18 @@ class Program
         {
             var release = await g.Repository.Release.GetLatest(repo.Id);
             var assets = release.Assets.Where(x => x.Name.Contains(".deb"));
+            var conf = new PkgConfig();
+            if (release.Assets.SingleOrDefault(i => i.Name.Equals("pkg.conf")) is ReleaseAsset a)
+            {
+                conf = await PkgConfig.GetConfig(a);
+            }
 
             if (!_pkgs.ContainsKey(repo.Name) || _pkgs[repo.Name] != release.Name)
             {
                 bool flag = true;
                 await Parallel.ForEachAsync(assets, async (a, token) =>
                 {
-                    if (!await DownloadAsset(a))
+                    if (!conf.IsExcluded(a) && !await DownloadAsset(a))
                     {
                         flag = false;
                     }
